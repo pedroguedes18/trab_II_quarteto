40bec458c80dc64a682819aace9bcf0c215e6921
pedroguedes18 <pedroleao18@gmail.com>
2016-10-29 21:15:36 +0100

    criei o vetor de pedidos pendentes e em execucao

--- Trabalho_ii/src/trabalho_ii/Gestor_Producao.java
+++ Trabalho_ii/src/trabalho_ii/Gestor_Producao.java
@@ -3,4 +3,11 @@ package trabalho_ii;
 
 public class Gestor_Producao {
     
+   private final String [] vetor_pedidos_pendentes = new String [15];           // cria um vetor de pedidos pendentes
+   private final String [] vetor_pedidos_execucao = new String [7];             // cria um vetor de pedidos execucao. Apenas tem sete pois sao o numero de celulas disponiveis.
+   
+   
+   
+   
+    
 }
