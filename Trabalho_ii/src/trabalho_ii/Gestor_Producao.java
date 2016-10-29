b028de0a819dfca0bc36a9b71c933d8608d27a0a
pedroguedes18 <pedroleao18@gmail.com>
2016-10-30 00:58:41 +0100

    adicionada a funcao para inserir no vetor_pedidos_pendentes e a funcao para ver se um vetor está cheio ou nao

--- Trabalho_ii/src/trabalho_ii/Gestor_Producao.java
+++ Trabalho_ii/src/trabalho_ii/Gestor_Producao.java
@@ -7,12 +7,12 @@ public class Gestor_Producao {
     private final String [] vetor_pedidos_execucao = new String [7];             // cria um vetor de pedidos execucao. Apenas tem sete pois sao o numero de celulas disponiveis.
    
     
-    private static Gestor_Producao instance;    // intancia que é da class Gestor_produção
+    private static Gestor_Producao instance;                                    // instância que é da class Gestor_produção
     
-    //private Gestor_Producao(){}               // ainda nao percebi para que é este método
+    //private Gestor_Producao(){}                                               // ainda nao percebi para que é este método
     
 	
-    public static Gestor_Producao getInstance()
+    public static Gestor_Producao getInstance()                                 // método para criar se ainda não foi criado uma instancia do Gestor de Produção
     {
 	if(instance==null)
         {
@@ -21,7 +21,46 @@ public class Gestor_Producao {
         
 	return instance;
     }
-	//Singleton
+        //Singleton
     
+    
+    //----------------------------------------------------------------------------------------------------------------------
+    // tentar que o gestor de produção receba o que lhe é enviado pelo Servidor UDP e o guarde no vetor de pedidos_pendentes
+    //----------------------------------------------------------------------------------------------------------------------
+    
+    private int ver_se_vetor_cheio (String [] vetor)
+    {
+        int pos = -1;
+        
+        for (int i=0; i<vetor.length-1; i++)                                    // percorre o vetor até encontrar uma posição vazia 
+        {
+            if(this.vetor_pedidos_pendentes[i] == null)
+            {
+                pos = i;
+                break;
+            }
+        }
+        
+        return pos;                                                             // se não encontrar nenhuma posicao vazia restorna -1;
+    }
+    
+    
+    public void insere_vetor_pedidos_pedentes(String pedido)
+    {
+        // vou ter de analizar o que tem na string pedido mas para já só guarda no vetor, e na posicao que está vazia.
+        
+        int pos = this.ver_se_vetor_cheio(vetor_pedidos_pendentes);             // se tem espaço é aqui guardado a posicao disponivel;
+        
+        if( pos > -1)
+        {
+            this.vetor_pedidos_pendentes[pos] = pedido;
+        }
+        
+        else
+        {
+            System.out.println("O vetor está cheio. Não é possivel adicionar mais pedidos");
+        }
+
+    }
  
 }
